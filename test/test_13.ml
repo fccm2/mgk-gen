4d6c62647ef92ebf45697f47254142c8b7b2727d
Florent Monnier <monnier.florent@gmail.com>
2024-10-08 11:40:48 +0200

    add the negate example

---
+++ test/test_13.ml
@@ -0,0 +1,9 @@
+open Magick
+
+let () =
+  let img1 = Magick.image_read "rose:" in
+  let img2 = Magick.image_edge img1 ~radius:1.2 in
+
+  Magick.image_negate img2;
+  Magick.image_display img2;
+;;
